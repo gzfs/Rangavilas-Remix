f70ddc5a42a9fc71a10c727836b1d5d0d0cf0853
gzfs <vismbs@pm.me>
2023-09-30 22:23:02 +0530

    Fixed Callback URL

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -14,13 +14,10 @@ remixAuthenticator.use(
       clientID: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       scope: ["email", "openid", "profile"],
-      callbackURL: `${
-        process.env.NODE_ENV === "production" ? "https" : "http"
-      }://${
+      callbackURL:
         process.env.NODE_ENV === "production"
-          ? "rangavilas.vercel.app"
-          : "localhost:3000"
-      }/auth/${SocialsProvider.GOOGLE}/callback`,
+          ? "https://rangavilas.vercel.app/auth/google/callback"
+          : "http://localhost:3000/auth/google/callback",
     },
     handleProfileCreateCallback
   )
